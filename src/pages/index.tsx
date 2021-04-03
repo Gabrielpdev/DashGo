20ad488d34864c35821a8b3ddc34d564b331188e
Gabriel Oliveira <gabriel9938@gmail.com>
2021-04-03 14:17:07 -0300

    pagina de signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,57 @@
-import Head from 'next/head'
+import { Flex, Button, Input, Stack, FormLabel, FormControl } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <h1>hello world</h1>
+  <Flex
+    w='100vw'
+    h='100vh'
+    align="center"
+    justify="center"
+  >
+    <Flex
+      as="form"
+      width="100%"
+      maxWidth={360}
+      bg="gray.800"
+      p="8"
+      borderRadius={8}
+      flexDir="column"
+    >
+      <Stack spacing="4">
+        <FormControl>
+          <FormLabel htmlFor="email">E-mail</FormLabel>
+          <Input
+            type="email"
+            name="email" 
+            focusBorderColor="pink.500"
+            bgColor="gray.900"
+            variant="filled"
+            _hover={{
+              bdColor: "gray.900"
+            }}
+            size="lg"
+          />
+        </FormControl>
+
+        
+        <FormControl>
+          <FormLabel htmlFor="password">Senha</FormLabel>
+          <Input
+            type="password"
+            name="password"
+            focusBorderColor="pink.500"
+            bgColor="gray.900"
+            variant="filled"
+            _hover={{
+              bdColor: "gray.900"
+            }}
+            size="lg"
+          />
+        </FormControl>
+      </Stack>
+
+      <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+    </Flex>
+   </Flex>
   )
 }
