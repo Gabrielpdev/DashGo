5adc7e340e03b1ccb7c0085471d096184fd95f44
Gabriel Oliveira <gabriel9938@gmail.com>
2021-04-03 14:39:35 -0300

    finalizando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Icon, Text } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Icon, Text, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header(){
   return(
@@ -47,6 +47,39 @@ export function Header(){
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+    
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack 
+          spacing="8" 
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="28" />
+          <Icon as={RiUserAddLine} fontSize="28" />
+        </HStack>
+      
+        <Flex
+          align="center"
+        >
+          <Box mr="4" textAlign="right">
+            <Text>Gabriel Pereira</Text>
+            <Text 
+              color="gray.300" 
+              fontSize="small"
+            >
+              gabriel9938@gmail.com</Text>
+          </Box>
+        
+          <Avatar size="md" name="Gabriel Pereira" src="https://github.com/gabrielpdev.png" />
+        </Flex>
       </Flex>
     </Flex>
   )
