312751c5e523cebd9f6bfd69c8cb6645a8575071
Gabriel Oliveira <gabriel9938@gmail.com>
2021-04-17 14:00:16 -0300

    listagem de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,7 +13,16 @@ export default function UserList(){
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json();
   
-    return data;
+    const users = data.users.map(user => ({
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+    }))
+
+    return users;
   });
 
   const isWideVersion = useBreakpointValue({
@@ -83,28 +92,30 @@ export default function UserList(){
                 </Thead>
 
                 <Tbody>
-                  <Tr>
-                    <Td px={["4","4","6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td px={["4","4","6"]}>
-                      <Box>
-                        <Text fontWeight="bold" >Gabriel Pereira</Text>
-                        <Text fontSize="sm" color="gray.300">gabriel9938@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && (
-                      <Td>
-                        04 de Abril, 2021
+                  {data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={["4","4","6"]}>
+                        <Checkbox colorScheme="pink" />
                       </Td>
-                    )}
-                    {isWideVersion && (
-                      <Td>
-                        <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>} >
-                        </Button>
+                      <Td px={["4","4","6"]}>
+                        <Box>
+                          <Text fontWeight="bold" >{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                        </Box>
                       </Td>
-                    )}
-                  </Tr>
+                      {isWideVersion && (
+                        <Td>
+                          {user.createdAt}
+                        </Td>
+                      )}
+                      {isWideVersion && (
+                        <Td>
+                          <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16"/>} >
+                          </Button>
+                        </Td>
+                      )}
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
 
